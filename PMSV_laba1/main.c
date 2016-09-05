492ca76679eebfb19bdde676d6bcdcb0b58d5abe
MaryLukyanova <bobbkrist@gmail.com>
2016-09-05 16:30:45 +0300

    Initial Commit

---
+++ PMSV_laba1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PMSV_laba1
+//
+//  Created by Admin on 05.09.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
